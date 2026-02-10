8a5e32a8049107b6e2c636fd98899a2580d688ec
DevXprashant207 <pt264doc@gmail.com>
2026-02-10 20:58:59 +0530

    add

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -6,9 +6,9 @@ import { signup,login,logout,updateProfile} from '../controllers/auth.controller
 
 router.post('/signup', signup);
 router.post('/login', login);
-router.put('/update-profile',protectRoute,updateProfile);
+router.put('/update-profile',updateProfile);
 router.post('/logout', logout);
-router.get("/check",protectRoute, (req,res)=>{ 
+router.get("/check", (req,res)=>{ 
     res.status(200).json(req.user);
 })
 
